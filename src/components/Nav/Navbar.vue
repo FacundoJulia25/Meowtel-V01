4587d8b6c8646152874cfe74df23f7cabcd70668
Facundo Juliá <facundodirie@gmail.com>
2023-05-04 03:49:00 -0300

    Navbar

---
+++ src/components/Nav/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <h1 class=""><a :href="`${home}.html`">{{ home }}</a></h1>
+        <ul class="nav-items">
+            <a v-for= "page in pages" :href="`${page}.html`" >{{ page }}</a>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default{
+    props:['casa'],
+    data(){
+        return {
+            home: 'Meowtel 😸',
+            kitties: 'Kitties',
+            pages:['Kitties', 'About Us', 'Contact']
+        }
+    }
+}
+</script>
